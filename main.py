0148ffef8d52791da6fdaaab0fb32aef276538d4
star1988 <star_awc2000@hotmail.com>
2026-08-14 04:29:18 +0500

    Connect SQLite database

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
+from sqlmodel import SQLModel, create_engine
+
+# Database setup
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+engine = create_engine(sqlite_url, echo=True)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
 
 app = FastAPI()
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
 
 @app.get("/")
 def read_root():
